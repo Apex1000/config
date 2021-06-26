d74b7817b52049204b97ac3cc59893022825c73b
siddythings <siddythings@gmail.com>
2021-06-26 22:23:43 +0530

    update

--- authentication/views.py
+++ authentication/views.py
@@ -29,6 +29,7 @@ from django.urls import reverse
 from django.shortcuts import redirect
 from django.http import HttpResponsePermanentRedirect
 from authentication.models import User
+from authentication.serializers import RegisterSerializer
 import os
 import json
 from django.http import HttpResponse, JsonResponse
@@ -60,46 +61,35 @@ class PhoneOTPVarifyView(generics.GenericAPIView):
 
 class RegisterView(generics.GenericAPIView):
 
-    serializer_class = auth_serializers.RegisterSerializer
-    # renderer_classes = (UserRenderer,)
+    serializer_class = RegisterSerializer
+    renderer_classes = (UserRenderer,)
 
     def post(self, request):
         user = request.data
-        # serializer = self.serializer_class(data=user)
-        # serializer.is_valid(raise_exception=True)
-        # serializer.save()
-        # user_data = serializer.data
-        # user = User.objects.get(email=user_data["email"])
-        # token = RefreshToken.for_user(user).access_token
-        # current_site = get_current_site(request).domain
-        # relativeLink = reverse("email-verify")
-        # absurl = "http://" + current_site + relativeLink + "?token=" + str(token)
-        # email_body = (
-        #     "Hi "
-        #     + user.username
-        #     + " Use the link below to verify your email \n"
-        #     + absurl
-        # )
-        # data = {
-        #     "email_body": email_body,
-        #     "to_email": user.email,
-        #     "email_subject": "Verify your email",
-        # }
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        user_data = serializer.data
+        user = User.objects.get(email=user_data["email"])
+        token = RefreshToken.for_user(user).access_token
+        current_site = get_current_site(request).domain
+        relativeLink = reverse("email-verify")
+        absurl = "http://" + current_site + relativeLink + "?token=" + str(token)
+        email_body = (
+            "Hi "
+            + user.username
+            + " Use the link below to verify your email \n"
+            + absurl
+        )
+        data = {
+            "email_body": email_body,
+            "to_email": user.email,
+            "email_subject": "Verify your email",
+        }
 
-        # Util.send_email(data)
-        if user.get("phone"):
-            return Response({"error":"Phone Number Requried!"}, status=status.HTTP_400_BAD_REQUEST)
-        user_obj = User.objects.filter(phone=user.get("phone"))
-        if user_obj:
-            return Response({"error":"Phone Number Already Exists!"}, status=status.HTTP_400_BAD_REQUEST)
+        Util.send_email(data)
+        return Response(user_data, status=status.HTTP_201_CREATED)
 
-        User.objects.create(
-                phone = user.get("phone"),
-                email = user.get("email"),
-                username = user.get("username"),
-                password = make_password(user.get("password"))
-            )
-        return Response({"status":"User Created!"}, status=status.HTTP_201_CREATED)
 
 class LoginAPIView(generics.GenericAPIView):
     serializer_class = auth_serializers.LoginSerializer
